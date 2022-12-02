694b08d83f5ae2b60560a06743c468db6b589572
Accretence <accretence@outlook.com>
2022-12-02 12:25:37 +0330

    Missing ID

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,6 @@
+import GoogleAnalytics from 'components/GoogleAnalytics'
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
-
-const isProduction = process.env.NODE_ENV === 'production'
-const googleID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
-
-const gtag = `https://www.googletagmanager.com/gtag/js?id=${googleID}`
-const gscript = {
-    __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${googleID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-}
 
 export default function Document() {
     return (
@@ -23,15 +9,7 @@ export default function Document() {
                 <link href="/favicon.ico" rel="shortcut icon" />
                 <meta content="#ffffff" name="theme-color" />
                 <meta content="#ffffff" name="msapplication-TileColor" />
-                {googleID && isProduction && (
-                    <>
-                        <Script src={gtag} async strategy="afterInteractive" />
-                        <Script
-                            dangerouslySetInnerHTML={gscript}
-                            strategy="afterInteractive"
-                        />
-                    </>
-                )}
+                <GoogleAnalytics />
                 <Script
                     src="https://accounts.google.com/gsi/client"
                     async
